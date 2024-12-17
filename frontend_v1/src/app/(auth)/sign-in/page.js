c77545d3c6f322e3789b23d15c6e81f2d11e8c24
monasefdev <monasef77@gmail.com>
2024-12-17 12:41:16 +0100

    refactor project

--- frontend_v1/src/app/(auth)/sign-in/page.js
+++
@@ -1,7 +0,0 @@
-import { SignInCard } from "@/features/auth/components/SignInCard";
-
-function SingInPage() {
-  return <SignInCard />;
-}
-
-export default SingInPage;
--- frontend_v1/src/app/(auth)/sign-up/page.js
+++
@@ -1,7 +0,0 @@
-import { SignUpCard } from "@/features/auth/components/SignUpCard";
-
-function SingInPage() {
-  return <SignUpCard />;
-}
-
-export default SingInPage;
